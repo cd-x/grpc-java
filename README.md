e4729ccb1ff92931260750059a964dd5e1593130
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2025-05-04 19:53:21 +0530

    unit test with TLS communication

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Just for demo purpose security certificate has been generated in src here.
 In actual env this should be read from secrets or should be managed by **Istio**.
+
+#### Before running maven build 
+
+1. you need to run ssl script to generate certificates as some tests use TLS
+2. make sure openssl is installed in your system
+3. go to _src/test/resources/ssl_ 
+4. run _chmod +x ssl.sh_ to make script executable
+5. run _./ssl.sh_  it would generate tls certificates. 
--- src/test/java/setup/TestSetup.java
+++ src/test/java/setup/TestSetup.java
@@ -2,14 +2,12 @@ package setup;
 
 import com.example.services.CalculatorServiceImpl;
 import com.example.services.GreetingServiceImpl;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -18,14 +16,17 @@ public class TestSetup {
     protected Server server;
     protected ManagedChannel managedChannel;
     protected static final int PORT = 50053;
-
+    private static final String CERT_PATH = "ssl/";
     @BeforeAll
     public void setupGrpcServer() throws IOException {
         server = ServerBuilder.forPort(PORT)
+                .useTransportSecurity(getCertificate("server.crt"), getCertificate("server.pem"))
                 .addService(new CalculatorServiceImpl())
                 .addService(new GreetingServiceImpl())
                 .build().start();
-        managedChannel = ManagedChannelBuilder.forAddress("localhost", PORT).usePlaintext().build();
+        ChannelCredentials credentials = TlsChannelCredentials.newBuilder().trustManager(getCertificate("ca.crt")).build();
+        managedChannel = Grpc.newChannelBuilderForAddress("localhost", PORT, credentials).build();
+        //managedChannel = ManagedChannelBuilder.forAddress("localhost", PORT).usePlaintext().build();
     }
     @AfterAll
     public void shutDownServer(){
@@ -33,4 +34,8 @@ public class TestSetup {
         if(Objects.nonNull(server)) server.shutdown();
     }
 
+    private File getCertificate(String name){
+        return new File(
+                Objects.requireNonNull(getClass().getClassLoader().getResource(CERT_PATH + name)).getFile());
+    }
 }
